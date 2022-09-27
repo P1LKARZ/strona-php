54c24d6e73a482dbecaa8ea2af71e752246212d3
P1LKARZ <91729018+P1LKARZ@users.noreply.github.com>
2022-09-27 14:35:58 +0200

    Add files via upload

---
+++ stronaM/index.php
@@ -0,0 +1,33 @@
+
+
+<!-- UTWORZYC PLIKI KTORE SA W LINKACH PRZY GLOWNYM MENU -->
+<!-- UTWORZYC PLIKI KTORE SA W LINKACH PRZY GLOWNYM MENU -->
+<!-- UTWORZYC PLIKI KTORE SA W LINKACH PRZY GLOWNYM MENU -->
+<html lang="pl">
+<head >
+
+	<meta charset="utf-8"/>
+		<title>strona phpp</title>
+			<meta name="keywords" content="informatyka, komputer, mysz"/> 
+				<meta name="description" content="strona o informatyce"/>
+					<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+						<link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<div id="lewo">
+lewo
+</div>
+<div id="srodek"><?php 
+echo "witaj świecie piękny";
+echo"<br>czesc";
+echo"<br>czesc";
+echo"<br>czesc<br>";
+?>
+
+
+</div>
+<div id="prawo">prawo</div>
+
+	
+</body>
