1f61d9e7db75b5fcca6e735957c11a91016741ce
P1LKARZ <91729018+P1LKARZ@users.noreply.github.com>
2022-10-11 14:31:03 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+
+
+<!-- UTWORZYC PLIKI KTORE SA W LINKACH PRZY GLOWNYM MENU -->
+<!-- UTWORZYC PLIKI KTORE SA W LINKACH PRZY GLOWNYM MENU -->
+<!-- UTWORZYC PLIKI KTORE SA W LINKACH PRZY GLOWNYM MENU -->
+<html lang="pl">
+<head >
+
+	<meta charset="utf-8"/>
+		
+			<meta name="keywords" content="informatyka, komputer, mysz"/> 
+				<meta name="description" content="strona o informatyce"/>
+					<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+						<link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+	
+</body>
+</html>
+<body>
+<div id="lewo">
+lewo
+</div>
+<div id="srodek">
+<?php 
+$jeden=10;
+$dwa="ebebe";
+$trzy="cena to $jeden";
+$php="$$ PHP cwiczymy string $$";
+echo "witaj świecie piękny";
+echo"<br>czesc";
+echo"<br>czesc";
+echo"<br>czesc<br>";
+
+
+
+echo "$jeden<br>";
+echo "$dwa<br>";
+echo "$trzy <br>";
+
+
+?>
+<form action="jeden.php" method="post">
+	Podaj imie<input type="text" name="imie" id="imie"><br>
+	Podaj nazwisko<input type="text" name="nazwisko" id="nazwisko"><br>
+	podaj liczbe <input type="number" name="numer" id="numer"><input type="number" name="numer1" id="numer1">
+	<input type="submit" value="wyslij" href="jeden.php">
+</form>
+
+</div>
+<div id="prawo">
+<?php	echo 
+"<style>
+h1{color:green;}
+h2{color:#42eb75;}
+</style>
+<h1> PHP to super język programowania</h1>";
+echo "<h2>$php</h2>";?>
+</div>
+
+
+	
+</body>
---
+++ jeden.php
@@ -0,0 +1,47 @@
+
+
+<!-- UTWORZYC PLIKI KTORE SA W LINKACH PRZY GLOWNYM MENU -->
+<!-- UTWORZYC PLIKI KTORE SA W LINKACH PRZY GLOWNYM MENU -->
+<!-- UTWORZYC PLIKI KTORE SA W LINKACH PRZY GLOWNYM MENU -->
+<html lang="pl">
+<head >
+
+	<meta charset="utf-8"/>
+		
+			<meta name="keywords" content="informatyka, komputer, mysz"/> 
+				<meta name="description" content="strona o informatyce"/>
+					<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+						<link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+	
+</body>
+</html>
+<body>
+<div id="lewo">
+lewo
+</div>
+<div id="srodek">
+
+<?php
+$nazwisko=$_POST['nazwisko'];
+$imie=$_POST['imie'];
+$numer=$_POST['numer'];
+$numer1=$_POST['numer1'];
+$suma=$numer+$numer1;
+echo<<<end
+$imie <br>
+$nazwisko<br>
+$suma<br>
+
+end;
+?>
+</div>
+<div id="prawo">
+
+</div>
+
+
+	
+</body>
